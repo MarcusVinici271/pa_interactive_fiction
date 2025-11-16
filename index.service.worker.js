df35fdb29023e27277297b414d6e3de6edefcf33
Marcus Albuquerque <marcus.valbuquerque@al.infnet.edu.br>
2025-11-16 20:33:19 -0300

    Aplicação dos botões de acessibilidade

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1763221611|1844946680';
+const CACHE_VERSION = '1763335957|54362899';
 /** @type {string} */
 const CACHE_PREFIX = 'Cronicas_FI-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
