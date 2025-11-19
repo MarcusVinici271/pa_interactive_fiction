37392265b1c5254105c537489b98f153f3d3d3c5
Marcus Albuquerque <marcus.valbuquerque@al.infnet.edu.br>
2025-11-18 22:36:05 -0300

    Refatoração de código de audios

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1763434665|7623338380';
+const CACHE_VERSION = '1763516106|101763649';
 /** @type {string} */
 const CACHE_PREFIX = 'Cronicas_FI-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
