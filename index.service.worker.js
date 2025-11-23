b97e33210765e12bf4a0255bb9172b494d04e196
Marcus Albuquerque <marcus.valbuquerque@al.infnet.edu.br>
2025-11-22 23:15:57 -0300

    Refatoração de código e melhorias

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1763691055|34787885';
+const CACHE_VERSION = '1763864123|25290247';
 /** @type {string} */
 const CACHE_PREFIX = 'Cronicas_FI-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
