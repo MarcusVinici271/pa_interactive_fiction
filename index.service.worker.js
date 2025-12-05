bdf421204a7a412739a5c554285b2632640765f3
Marcus Albuquerque <marcus.valbuquerque@al.infnet.edu.br>
2025-12-04 22:16:54 -0300

    Refatoração de código e finalização de código

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1764808325|186170980';
+const CACHE_VERSION = '1764897322|19312478';
 /** @type {string} */
 const CACHE_PREFIX = 'Cronicas da Lagr-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
